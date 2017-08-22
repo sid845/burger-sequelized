91df6b81edbbbadd50db89446c553b100e09da20
Siddharth Patel <siddharthpatel845@gmail.com>
2017-08-22 10:56:07 -0400

    database synced

---
+++ models/newBurger.js
@@ -0,0 +1,17 @@
+module.exports = function(sequelize, DataTypes){
+  var NewBurger = sequelize.define("NewBurger",{
+    burger_name:{
+      type:DataTypes.STRING,
+      allowNull:false,
+      validate:{
+        len:[1]
+      }
+    },
+    devoured:{
+      type:DataTypes.TINYINT,
+    }
+    date:{
+      type:DataTypes.DATE,
+    }
+  });
+}
