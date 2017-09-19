13d79e64c2af81da1f5b47cac740b3a09e5baf74
Siddharth Patel <siddharthpatel845@gmail.com>
2017-09-19 13:34:46 -0400

    sequelize init

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,12 @@
+
 CREATE DATABASE burgers_db;
 USE burgers_db;
+
+CREATE TABLE burgers
+(
+	`id` integer (10) auto_increment NOT NULL,
+	`burger_name` varchar(45) NOT NULL,
+    `devoured` BOOLEAN DEFAULT false,
+	`date` TIMESTAMP,
+	PRIMARY KEY (id)
+);
